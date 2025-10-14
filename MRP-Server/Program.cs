88e7e45c2d476864c5e917b336c7df2d39840e7d
KlimaRobin <if25b233@technikum-wien.at>
2025-10-14 14:51:57 +0200

    implemented JAB for compile time DI

--- MRP-Server/Program.cs
+++ MRP-Server/Program.cs
@@ -4,47 +4,18 @@ using MRP_DAL.Repositories;
 using MRP_Server.Http;
 using MRP_Server.Http.Controllers;
 using MRP_Server.Services;
+using Npgsql;
 
 namespace MRP_Server
 {
-    internal class Program
+    internal class Program 
     {
         static async Task Main(string[] args)
         {
-            var connection = InitializeDatabase();
+            var provider = new ServiceProvider();
 
-            var serverAuthService = BuildServices(connection);
-
-            var userController = new UserController(serverAuthService);
-            var mediaController = new MediaController(new MediaEntryRepository(connection), serverAuthService);
-
-            var server = new HttpServer(userController, mediaController);
-
+            var server = provider.GetService<HttpServer>();
             await server.StartAsync("http://localhost:8080/");
-
-        }
-
-        private static Npgsql.NpgsqlConnection? InitializeDatabase()
-        {
-            var db = new DatabaseConnection();
-            db.StartConnection();
-
-            if (db.Connection is null)
-            {
-                return null;
-            }
-
-            return db.Connection!;
-        }
-
-        private static ServerAuthService BuildServices(Npgsql.NpgsqlConnection connection)
-        {
-            var userRepo = new UserRepository(connection);
-            var credentialsRepo = new CredentialsRepository(connection);
-            var authService = new AuthService(credentialsRepo, userRepo);
-            var tokenManager = new TokenManager();
-
-            return new ServerAuthService(authService, userRepo, tokenManager);
         }
     }
 }
---
+++ MRP-Server/ServiceProvider.cs
@@ -0,0 +1,46 @@
+﻿using Jab;
+using MRP_DAL;
+using MRP_DAL.Interfaces;
+using MRP_DAL.Repositories;
+using MRP_Server.Http;
+using MRP_Server.Http.Controllers;
+using MRP_Server.Services;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRP_Server
+{
+    [ServiceProvider]
+
+    [Singleton<DatabaseConnection>()]
+    [Singleton(typeof(NpgsqlConnection), Factory = nameof(CreateConnection))]
+
+    [Singleton<UserRepository>()]
+    [Singleton<CredentialsRepository>()]
+    [Singleton<IMediaEntryRepository, MediaEntryRepository>]
+
+    [Singleton<AuthService>()]
+    [Singleton<TokenManager>()]
+    [Singleton<ServerAuthService>()]
+
+    [Transient<UserController>()]
+    [Transient<MediaController>()]
+
+    [Singleton<HttpServer>()] 
+    public partial class ServiceProvider {
+        private NpgsqlConnection CreateConnection()
+        {
+            var db = new DatabaseConnection();
+            db.StartConnection();
+
+            if (db.Connection is null)
+                throw new InvalidOperationException("Database connection failed.");
+
+            return db.Connection!;
+        }
+    }
+}
